ab7f681afd24b16bd591de95bfecb888599bae2c
Manuel Muñoz <manuel.mz.mn@gmail.com>
2023-12-22 18:47:46 +0100

    elimino el token

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -12,17 +12,11 @@ export const fetchCharacters = async (page: number, name?: string) => {
   return response.json();
 };
 
-const token = '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'
-
 
 export const fetchOrg = async (organizacion: string, currentPage: number, perPage: number) => {
   const url = (`https://api.github.com/orgs/${organizacion}/members?page=${currentPage}&per_page=${perPage}`)
 
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  });
+  const response = await fetch(url);
 
   if (response.status !== 200) {
     throw new Error(`Error: ${response.status}`);
